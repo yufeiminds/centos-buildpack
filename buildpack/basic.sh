3d4855be34bce9700124f19d9889aa10357ec23a
Yufei Li <yufeiminds@gmail.com>
2015-03-30 14:36:04 +0000

    Add EPEL repo

--- buildpack/basic.sh
+++ buildpack/basic.sh
@@ -7,7 +7,11 @@ curl -L http://install.ohmyz.sh | sh
 tips "Install Docker."
 yum -y install docker
 
-tips "Install Development Tools"
+tips "Install Development Tools."
 yum -y groupinstall "Development Tools"
+
+tips "Install Development Library."
 yum -y install gcc gcc-c++ autoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers
 
+tips "Add EPEL(Extra Packages for Enterprise Linux) repo."
+yum install epel-release
--- buildpack/nosql.sh
+++ buildpack/nosql.sh
@@ -12,8 +12,6 @@ service mongod start
 
 tips "MongoDB installed Successful"
 
-rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
-yum install redis -y
+yum install redis
 
 tips "Redis installed Successful"
